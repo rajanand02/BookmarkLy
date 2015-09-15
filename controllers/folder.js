8883bc02ed058026f60234d80ad39a9586dd1b64
rajanand <rajanand0202@gmail.com>
2015-09-15 20:32:21 +0530

    add rest end points - get, put and delete for folder collection

--- controllers/folder.js
+++ controllers/folder.js
@@ -20,5 +20,40 @@ exports.getAllFolder = function (req, res) {
     }else{
     }
     res.json({ folders: folders});
-  })
+  });
+};
+
+exports.getFolder = function (req, res) {
+  var id = req.params.folder_id;
+  Folder.findById(id, function (err, folder) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({ folder: folder});
+    }
+  });
+};
+
+exports.updateFolder  = function (req, res) {
+  var id = req.params.folder_id;
+  Folder.findByIdAndUpdate(id, {$set: req.body}, function (err, folder) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({ folder: folder});
+    }
+  });
+};
+
+exports.deleteFolder  = function (req, res) {
+  var id = req.params.folder_id;
+  Folder.findByIdAndRemove(id, {$set: req.body}, function (err, folder) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.status(200).send({
+        message: "Folder Deleted successfully."
+      });
+    }
+  });
 };
--- routes/folderRoute.js
+++ routes/folderRoute.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 var folderController = require('../controllers/folder.js');
 
-router.route('/posts')
+router.route('/folders')
   .post(folderController.postFolder)
   .get( folderController.getAllFolder);
+
+
+router.route('/folders/:folder_id')
+  .get(folderController.getFolder)
+  .put(folderController.updateFolder)
+  .delete(folderController.deleteFolder);
 
 module.exports = router;
